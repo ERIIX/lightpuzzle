98abf5b9aa4ab38ad7917acd7234fc6e9f62c66f
ERIIX M. Blaike <eriix@eriix.org>
2019-06-01 00:46:21 -0400

    Updated binary and improved output.

--- lightpuzzle.lisp
+++ lightpuzzle.lisp
@@ -2,35 +2,10 @@
 
 (in-package #:lightpuzzle)
 
-#||
-;;; Whoops.  Complicating the problem a bit...
-(defun fix (index max)
-  "Fix array indicies so they don't over- or under-flow.
-   Implements wrap-arond to fix off-by-one.  Fails with larger changes."
-  (cond
-    ((<= 0 index max) index)
-    ((< index 0) max)
-    (t 0)))
-
-(defun switch (x y source)
-  "Since we use even/odd for states, just increment to toggle."
-  (loop 
-    with state = (alexandria:copy-array (cdr source))
-    with dimensions = (array-dimensions state)
-    with dx = (1- (car dimensions)) and dy = (1- (cadr dimensions))
-    for i in '((-1 . 0) (0 . -1) (0 . 0) (0 . 1) (1 . 0))
-    for xx = (fix (+ x (car i)) dx)
-    and yy = (fix (+ y (cdr i)) dy)
-    do (incf (aref state xx yy))
-    finally (return (cons (cons (cons x y) (car source)) state))))
-||#
-
 (defun switch (x y source dx dy)
   "Since we use even/odd for states, just increment to toggle."
   (loop 
     with state = (alexandria:copy-array (car source))
-;    with dimensions = (array-dimensions state)
-;    with dx = (car dimensions) and dy = (cadr dimensions)
     for i in '((-1 . 0) (0 . -1) (0 . 0) (0 . 1) (1 . 0))
     for xx = (+ x (car i))
     and yy = (+ y (cdr i))
@@ -39,30 +14,44 @@
 
 (defun evaluate (source)
   "All odd?  Done."
-  ;; No need to be sbcl-spcific here, I suppose...
-  ; (every #'oddp (sb-ext:array-storage-vector (car source)))
   (every #'oddp (make-array (reduce #'* (array-dimensions (car source)))
                             :displaced-to (car source))))
 
 (defun solve (state
-               &optional
-               (work (list (list state)))
-               (head (car work))
-               (tail work)
-               (dimensions (array-dimensions state))
-               (dx (car dimensions))
-               (dy (cadr dimensions)))
-  (loop
-     while (not (evaluate head))
-     do (loop
-           for i from 0 below dx
-           do (loop
-                 for j from 0 below dy
-                 for new = (switch i j head dx dy)
-                 do (rplacd tail (cons new nil))
-                 do (setf tail (cdr tail))))
-     do (setf head (pop work))
-     finally (return head)))
+              &optional
+              (work (list (list state)))
+              (head (car work))
+              (tail nil)
+              (dimensions (array-dimensions state))
+              (dx (car dimensions))
+              (dy (cadr dimensions)))
+  (if (evaluate head)
+      head
+      (loop 
+        named main
+        do (loop
+             for i from 0 below dx
+             do (loop
+                  for j from 0 below dy
+                  for new = (switch i j head dx dy)
+                  do (if (evaluate new)
+                         (return-from main new)
+                         (unless (find (list i j) (cdr head) :test #'equal)
+                           (if tail
+                               (progn 
+                                 (rplacd tail (list new))
+                                 (setf tail (cdr tail)))
+                               (progn
+                                 (rplacd work (list new))
+                                 (setf tail (cdr work))))))))
+        do (setf head (pop work)))))
+
+(defun interpret (source)
+  (loop with out = (make-array (array-dimensions (car source))
+                               :initial-element 0)
+        for move in (cdr source)
+        do (setf (aref out (car move) (cadr move)) 1)
+        finally (return out)))
 
 (defun lightpuzzle ()
   (let* ((col (progn (princ "How many columns? ") (force-output) (read)))
@@ -76,5 +65,10 @@
              (loop repeat row collect (loop repeat col collect (read)))))
          (result (solve (make-array (list row col)
                                     :initial-contents contents))))
-    (princ "Okay.  Hit the following spaces: ")
-    (format t "~{~{(~*~A, ~2:*~A~*)~}~#[.~:;, ~]~}~%" (cdr result))))
+    (princ "Okay.  Hit these spaces:")
+    (terpri)
+    (loop with output = (interpret result)
+          for i below col
+          do (loop for j below row
+                   do (format t "~a " (aref output i j)))
+          (terpri))))
